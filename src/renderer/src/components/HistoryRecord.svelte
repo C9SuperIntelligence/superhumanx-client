10ab218e6d29b3a0d9544051364c135071eb2891
Filip Holčík <filip.holcik.official@gmail.com>
2024-06-30 19:46:24 +0200

    feat: added transition to HistoryRecord

--- src/renderer/src/components/HistoryRecord.svelte
+++ src/renderer/src/components/HistoryRecord.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
   import type { TrackingRecord } from '../../../types/index.ts'
+  import { fade } from 'svelte/transition'
   import Timer from './Timer.svelte'
 
   export let record: TrackingRecord
 </script>
 
-{#key record}
-  <div class="flex flex-wrap justify-between rounded p-2 m-1 bg-surface-900 text-white">
-    <div>{record.label}</div>
-    <div><Timer start={record.startTime} end={record.endTime} /></div>
-  </div>
-{/key}
+<div class="flex flex-wrap justify-between rounded p-2 m-1 bg-surface-900 text-white" in:fade>
+  <div>{record.label}</div>
+  <div><Timer start={record.startTime} end={record.endTime} /></div>
+</div>
